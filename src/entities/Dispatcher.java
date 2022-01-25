2293055e370d9047978ddf8ae8e32354989fecab
saleh_sh <shirvani.saleh79@gmail.com>
2022-01-25 19:14:47 +0330

    shortest job first

--- src/entities/Dispatcher.java
+++ src/entities/Dispatcher.java
@@ -5,6 +5,32 @@ public class Dispatcher {
     
 
     public void sjfDispatch() {
+        
+        Processor processor = Processor.INSTANCE;
+        Scheduler scheduler = processor.getScheduler();
+        Task currentTask = null;
+
+        while (processor.getReadyTaskCount() != 0) {
+            if (!processor.isBusy()) {
+                // selected task
+                currentTask = scheduler.sjfScheduling();
+
+                // assign cpu to task
+                processor.assign(currentTask);
+
+                currentTask.run();
+            }
+
+            currentTask.increaseTotalTime();
+            if (currentTask.isTerminated()) {
+                currentTask.terminate();
+                processor.unassign();
+            }
+
+            OutputGenerator.generateOutput();
+            processor.increaseTime();
+        }
+
     }
 
     public void rrDispatch() {
@@ -37,4 +63,5 @@ public class Dispatcher {
             processor.increaseTime();
         }
     }
+    
 }
--- src/entities/Scheduler.java
+++ src/entities/Scheduler.java
@@ -1,12 +1,20 @@
 package entities;
-
 import java.util.Queue;
 
 public class Scheduler {
 
-
     public Task sjfScheduling() {
-        return null;
+        Queue<Task> readyQueue = Processor.INSTANCE.getReadyQueue();
+        Task shortestJob = null;
+        int minDuration = Integer.MAX_VALUE;
+        for (Task t : readyQueue) {
+            if (t.DURATION < minDuration) {
+                minDuration = t.DURATION;
+                shortestJob = t;
+            }
+        }
+        readyQueue.remove(shortestJob);
+        return shortestJob;
     }
 
     public Task rrScheduling() {
